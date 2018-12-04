1ceb4e7841f47b71f31282a258b4a8bb408e17f4
Caleb Campbell <38718983+MrChido@users.noreply.github.com>
2018-12-04 15:03:22 -0600

    JS file added, sub-sets defined

---
+++ tgJS.js
@@ -0,0 +1,9 @@
+// Time Period Creature sets
+
+var pastCreature = [Lost, Stalker, Beast, Ancient, Golem, Elemental]
+
+var presentCreature = [Assasin, Paladin, Collector, Tactition, Theif, Warrior]
+
+var futureCreature = [Wanderer, Soldier, Mech, Generator, Engineer, Program]
+
+const assign = getElementById("Populate");
