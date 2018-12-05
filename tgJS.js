b00bb4e98f7f174377a045904c69795c0ca7d960
Caleb Campbell <38718983+MrChido@users.noreply.github.com>
2018-12-05 06:29:58 -0600

    still trying to get some dispaly
    slight progress has been made, the function is no longer broken :/ But my Array is.

--- tgJS.js
+++ tgJS.js
@@ -5,13 +5,17 @@ var pastCreature = [Lost, Stalker, Beast, Ancient, Golem, Elemental]
 var presentCreature = [Assasin, Paladin, Collector, Tactition, Theif, Warrior]
 
 var futureCreature = [Wanderer, Soldier, Mech, Generator, Engineer, Program]
+//html callouts
 
+var pp1 = a1.value 
+
+const ppa1 = getElementById("a1")
 const assign = getElementById("Populate");
 
-function(randomPast) {
+function randomPast() {
 	return pastCreature[Math.floor(Math.random()*pastCreature.length)];
 	
 }
 assign.addEventListener('click', ()=>{
-	p1.value=randomPast;
+	pp1=randomPast;
 });
