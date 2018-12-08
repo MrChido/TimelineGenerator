6544f4dff3b332f65548a425037f1701db6140fe
Caleb Campbell <38718983+MrChido@users.noreply.github.com>
2018-12-08 06:59:50 -0600

    started to develop the look of the web app.
    Just started to develop the app by giving the 3 time periods unique colors, and rearanging the elements of the web app to prepare for  the more daunting aspects of designing the look of the app.

--- tgJS.js
+++ tgJS.js
@@ -6,16 +6,39 @@ var presentCreature = [ 'Assasin', 'Paladin', 'Collector', 'Tactition', 'Theif',
 
 var futureCreature = [ 'Wanderer', 'Soldier', 'Mech', 'Generator', 'Engineer', 'Program'] 
 //html callouts
+const pastOne =document.getElementById('a1');
+const pastTwo= document.getElementById('a2');
+const pastThree= document.getElementById('a3');
 
-var pp1 = a1.value 
+const presentOne=document.getElementById('r1');
+const presentTwo=document.getElementById('r2');
+const presentThree=document.getElementById('r3');
 
-const ppa1 = document.getElementById("a1");
+const futureOne=document.getElementById('f1');
+const futureTwo=document.getElementById('f2');
+const futureThree=document.getElementById('f3');
+
 const assign = document.getElementById("Populate");
 
-function randomPast(pastCreature) {
+//function zone
+function randomPast() {
 	return pastCreature[Math.floor(Math.random()*pastCreature.length)];
-	
+	}
+function randomPresent(){
+return presentCreature[Math.floor(Math.random()*presentCreature.length)];
+}	
+function randomFuture(){
+return futureCreature[Math.floor(Math.random()*futureCreature.length)];
 }
+//action area	
 assign.addEventListener('click', ()=>{
-	a1.value=randomPast;
+	a1.value=randomPast();
+	a2.value=randomPast();
+	a3.value=randomPast();
+	r1.value=randomPresent();
+	r2.value=randomPresent();
+	r3.value=randomPresent();
+	f1.value=randomFuture();
+	f2.value=randomFuture();
+	f3.value=randomFuture();
 });
